c0fd8abd22ddd180eef09e8a360abd9743f04339
TimothyXia9 <77732791+TimothyXia9@users.noreply.github.com>
2023-09-13 00:08:39 +0800

    admin page changed for blog

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from .models import BlogPost
 # Register your models here.
 
 
-admin.site.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")  # 在列表中显示的字段
+    ordering = ("created_at",)
+
+
+# 注册你的模型和 Admin 类
+admin.site.register(BlogPost, BlogPostAdmin)
